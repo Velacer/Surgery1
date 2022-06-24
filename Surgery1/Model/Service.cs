a31343442d4e13ec946a8feb293fe57f404156ef
Velacer <vlad.gykov2003_2@mail.ru>
2022-06-24 12:07:26 +0300

    Classwork

--- Surgery1/Model/Service.cs
+++ Surgery1/Model/Service.cs
@@ -14,6 +14,7 @@ namespace Surgery1.Model
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public decimal Price { get; set; }
         public int? Category { get; set; }
 
